ffc8e666e597c69f2e62d9e872f48acadc12d9e0
Nikita Marinosyan <nikita.marinosyan@gmail.com>
2018-05-26 22:40:44 +0300

    Found bug when watching .less file with imports

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,49 @@
-'use strict';
+let gulp         = require("gulp"),
+    less         = require("gulp-less"),
+    autoprefixer = require("gulp-autoprefixer"),
+    cleanCSS    = require("gulp-clean-css"),
+    sourcemaps      = require("gulp-sourcemaps");
+    notify      = require("gulp-notify");
+    browserSync  = require("browser-sync").create();
 
-const gulp         = require('gulp'),
-      del          = require('del'),
-      autoprefixer = require('gulp-autoprefixer'),
-      less         = require('gulp-less'),
-      cleanCSS     = require('gulp-clean-css'),
-      gulpIf       = require('gulp-if'),
-      sourceMaps   = require('gulp-sourcemaps'),
-      browserSync  = require('browser-sync').create();
+gulp.task("index", function() {
+    return gulp.src("src/index.html")
+        .pipe(gulp.dest("dist"));
+});
 
-const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
+gulp.task("images", function() {
+    return gulp.src("src/assets/themes/base/images/**/*.*", {base: "src/assets/themes/base"})
+        .pipe(gulp.dest("dist"));
+});
 
-gulp.task('less', () => {
-    return gulp.src('src/assets/themes/base/base.less')
-        .pipe(gulpIf(isDevelopment, sourceMaps.init()))
+gulp.task("less", function() {
+    return gulp.src("src/assets/themes/base/styles/base.less")
+        //.pipe(sourcemaps.init())
         .pipe(less())
-        .on('error', onLessError)
+        .on("error", function() {
+            console.log('error in less')
+        })
         .pipe(autoprefixer({
             browsers: [
-                'last 2 versions',
-                'safari 5',
-                'ie 10',
-                'ie 11'
+                "last 2 versions",
+                "safari 5",
+                "ie 10",
+                "ie 11"
             ],
             cascade: true
         }))
+        .on("error", notify.onError({
+            title: "style"
+        }))
         .pipe(cleanCSS())
-        .pipe(gulpIf(isDevelopment, sourceMaps.write()))
-        .pipe(gulp.dest('prod'));
+        //.pipe(sourcemaps.write())
+        .pipe(gulp.dest("dist"));
 });
 
-gulp.task('images', () => {
-    return gulp.src('src/assets/themes/base/images/**/*.*', {base: 'src/assets/themes/base', since: gulp.lastRun('images')})
-        .pipe(gulp.dest('prod'));
+gulp.task("watch", function() {
+    gulp.watch("src/index.html", gulp.series("index"));
+    gulp.watch("src/assets/themes/base/images/**/*.*", gulp.series("images"));
+    gulp.watch("src/assets/themes/base/styles/**/*.less", gulp.series("less"));
 });
 
-gulp.task('index', () => {
-    return gulp.src('src/index.html', {since: gulp.lastRun('index')})
-        .pipe(gulp.dest('prod'));
-});
-
-gulp.task('clean', () => {
-    return del('prod');
-});
-
-gulp.task('build', gulp.series('clean', gulp.parallel('less', 'images', 'index')));
-
-gulp.task('watch', () => {
-    gulp.watch('src/assets/themes/base/base.less', gulp.series('less'));
-    gulp.watch('src/assets/themes/base/images/**/*.*', gulp.series('images'));
-    gulp.watch('src/index.html', gulp.series('index'));
-});
-
-gulp.task('serve', function() {
-
-    browserSync.init({
-        server: 'prod'
-    });
-
-    browserSync.watch('prod/**/*.*').on('change', browserSync.reload);
-});
-
-gulp.task('dev', gulp.series('build', gulp.parallel('watch', 'serve')));
-
-/***HELPERS FUNCTIONS***/
-function onLessError(error) {
-    console.error(error.message);
-    this.emit('end');
-}
+gulp.task("default", gulp.series(gulp.parallel("index", "less", "images"), "watch"));
