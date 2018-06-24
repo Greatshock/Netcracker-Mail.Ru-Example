8e62ccdaeec098f8ceb7dbcf27855f6af9da6782
Nikita Marinosyan <nikita.marinosyan@gmail.com>
2018-06-24 14:33:05 +0300

    Refactored the code after review

--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -1,13 +1,11 @@
-"use strict";
-
 // FIRST TASK ON TYPESCRIPT
 import { ModalComponent } from './modal.component';
 import { SelectComponent } from './select.component'
 
-class AppComponent {
+export class AppComponent {
 
-    private modal = new ModalComponent();
-    private select = new SelectComponent();
+    private modal: ModalComponent = new ModalComponent();
+    private select: SelectComponent = new SelectComponent();
 
     constructor() {
         this.modal.modalLog();
@@ -15,43 +13,20 @@ class AppComponent {
     }
 }
 
-let app = new AppComponent();
+let app: AppComponent = new AppComponent();
 
 /* --------------------------------------------- CONFIGURATIONS ----------------------------------------------------- */
 const DYNAMIC_WIDGETS_IMPLEMENTATION: number = 1; // Switch to 0 to use implementation with fetch
-window.onscroll = scrollYHandlerUsingAsyncAwait;
 
-// HELPER FUNCTIONS
-function getRandomColor(): string {
-    let letters: string = "0123456789ABCDEF";
-    let color:string = "#";
-    for (let i = 0; i < 6; i++)
-        color += letters[Math.floor(Math.random() * 16)];
-
-    return color;
-}
-function createWidget(): HTMLDivElement {
-    let widget: HTMLDivElement = document.createElement("div");
-    widget.className = "widget";
-    widget.style.height = "100px";
-    widget.style.display = "flex";
-    widget.style.justifyContent = "center";
-    widget.style.alignItems = "center";
-    widget.style.backgroundColor = getRandomColor();
-
-    return widget;
-}
-function resolveAfter2Seconds(): Promise<string> {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve('widgets fetched');
-        }, 2000);
-    });
+if (DYNAMIC_WIDGETS_IMPLEMENTATION) {
+    window.addEventListener("scroll", scrollYHandlerUsingAsyncAwait);
+} else {
+    window.addEventListener("scroll", scrollYHandlerUsingFetch);
 }
 
-/* -------------------------------------- IMPLEMENTATIONS OF DYNAMIC WIDGETS ---------------------------------------- */
+/* ------------------------------- IMPLEMENTATIONS OF DYNAMIC WIDGETS --------------------------- */
 // Implementation using fetch
-function scrollYHandlerUsingFetch() {
+function scrollYHandlerUsingFetch(): void {
     // Get current page content height (cross-browser approach)
     let scrollHeight: number = Math.max(
         document.body.scrollHeight, document.documentElement.scrollHeight,
@@ -77,13 +52,8 @@ function scrollYHandlerUsingFetch() {
                     }
 
                     // Create and append widgets
-                    let line: HTMLElement = <HTMLElement>document.querySelector(".page__line._fourth");
-                    line.insertAdjacentElement(<InsertPosition>"beforeEnd", createWidget());
-                    line.insertAdjacentElement(<InsertPosition>"beforeEnd", createWidget());
-                    line.insertAdjacentElement(<InsertPosition>"beforeEnd", createWidget());
-                    line.insertAdjacentElement(<InsertPosition>"beforeEnd", createWidget());
-                    line.insertAdjacentElement(<InsertPosition>"beforeEnd", createWidget());
-                    line.insertAdjacentElement(<InsertPosition>"beforeEnd", createWidget());
+                    let line: HTMLElement = document.querySelector(".page__line._fourth");
+                    line.insertAdjacentElement("beforeend", createWidgetsBlock());
 
                     // Hide loader
                     loader.style.visibility = "hidden";
@@ -113,17 +83,13 @@ async function scrollYHandlerUsingAsyncAwait() {
         loader.style.opacity = "1";
 
         // Fetch new content from the server
-        let result: void = await resolveAfter2Seconds()
+        let result: void = await fetchWidgets(2000)
             .then(function(result) {
 
                 // Create and append widgets
-                let line: HTMLElement = <HTMLElement>document.querySelector(".page__line._fourth");
-                line.insertAdjacentElement(<InsertPosition>"beforeEnd", createWidget());
-                line.insertAdjacentElement(<InsertPosition>"beforeEnd", createWidget());
-                line.insertAdjacentElement(<InsertPosition>"beforeEnd", createWidget());
-                line.insertAdjacentElement(<InsertPosition>"beforeEnd", createWidget());
-                line.insertAdjacentElement(<InsertPosition>"beforeEnd", createWidget());
-                line.insertAdjacentElement(<InsertPosition>"beforeEnd", createWidget());
+                let line: HTMLElement =
+                    <HTMLElement>document.querySelector(".page__line._fourth");
+                line.insertAdjacentElement("beforeend", createWidgetsBlock());
 
                 // Hide loader
                 loader.style.visibility = "hidden";
@@ -134,5 +100,58 @@ async function scrollYHandlerUsingAsyncAwait() {
             .catch(alert);
     }
 }
-/* ----------------------------------- END OF IMPLEMENTATIONS OF DYNAMIC WIDGETS ------------------------------------ */
+/* --------------------------- END OF IMPLEMENTATIONS OF DYNAMIC WIDGETS ------------------------ */
 
+
+
+// HELPERS
+function fetchWidgets(timeout: number): Promise<string> {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('widgets fetched');
+        }, timeout);
+    });
+}
+
+function getRandomColor(): string {
+    let letters: string = "0123456789ABCDEF",
+        color:string = "#";
+
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+}
+
+function applyStyles(element: HTMLElement, styles: {[style: string]: string}): void {
+    for (let styleName in styles) {
+        if (styles.hasOwnProperty(styleName)) {
+            element.style[<any>styleName] = styles[styleName];
+        }
+    }
+}
+
+function createWidgetsBlock(): HTMLDivElement {
+    let widgetsBlock: HTMLDivElement = document.createElement("div");
+    widgetsBlock.style.marginBottom = "80px";
+
+    for (let i = 0; i < 6; i++) {
+        let widget: HTMLDivElement = document.createElement("div");
+
+        let styles = {
+            className: "widget",
+            height: "100px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: getRandomColor(),
+            marginBottom: "10px"
+        };
+        applyStyles(widget, styles);
+
+        widgetsBlock.insertAdjacentElement("beforeend", widget);
+    }
+
+    return widgetsBlock;
+}
